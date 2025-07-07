92e94263aa7399f0c3b370a58c49d8c5840b21ee
angiePedraza <minijin1234567@gmail.com>
2025-07-07 07:42:24 -0500

    Begin Repository patterns

---
+++ Orders/Orders.Frontend/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+namespace Orders.Frontend.Repositories
+{
+
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Response = response;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public T? Response { get; }
+        public bool Error { get; }
+        public HttpResponseMessage HttpResponseMessage { get; }
+
+        public async Task<string?> GetErrorMessageAsycn()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+
+            var statusCode = HttpResponseMessage.StatusCode;
+            if (statusCode == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+            if (statusCode == HttpStatusCode.Unauthorized)
+            {
+                return "Tiene que estar lagueado para ejecutar esta operacion";
+            }
+            if (statusCode == HttpStatusCode.Forbidden)
+            {
+                return "No tiene permisos para hacer esta operación";
+            }
+            return "Ha ocurrido un error inesperado";
+        }
+    }
+}
+
---
+++ Orders/Orders.Frontend/Repositories/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Orders.Frontend.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> GetAsycn<T>(string url);
+        Task<HttpResponseWrapper<object>> PostAsycn<T>(string url, T model);
+        Task<HttpResponseWrapper<TActionResponse>> PostAsycn<T, TActionResponse>(string url, T model);
+    }
+}
