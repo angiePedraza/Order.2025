ba0bda9df2611f67c22c052868d8b964e1b93c49
angiePedraza <minijin1234567@gmail.com>
2025-07-14 16:00:42 -0500

    Segurity

---
+++ Orders/Orders.Frontend/AuthenticationProviders/AuthenticationProviderTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Orders.Frontend.AuthenticationProviders
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(3000);
+            var anonimous = new ClaimsIdentity();
+            var user = new ClaimsIdentity(authenticationType: "test");
+            var admin = new ClaimsIdentity(new List<Claim>
+    {
+        new Claim("FirstName", "Angie"),
+        new Claim("LastName", "Pedraza"),
+        new Claim(ClaimTypes.Name, "angie.pedraza@gmail.com"),
+          new Claim(ClaimTypes.Role, "Admin")
+    },
+    authenticationType: "test");
+
+
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(user)));
+        }
+    }
+}
--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -1,7 +1,9 @@
 using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Orders.Frontend;
+using Orders.Frontend.AuthenticationProviders;
 using Orders.Frontend.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,5 +13,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7098/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
+
 
 await builder.Build().RunAsync();
